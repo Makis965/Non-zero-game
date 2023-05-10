50580c8fd1db8bb13489c8863f6121ca9a774a8d
Marcel Ochocki <99407020+Makis965@users.noreply.github.com>
2023-05-10 21:06:39 +0200

    Update funkcja.m

--- funkcja.m
+++ funkcja.m
@@ -1,4 +1,4 @@
-function [eql] = funkcja(P1,P2)
+function [eql] = non_zero_game(P1,P2)
     eql = {}; 
     for i = 1:length(P1)
         if any(size(P1{i}) == 1)
@@ -10,32 +10,26 @@ function [eql] = funkcja(P1,P2)
                                           %the id cost of the first player as
             cost_p1 = P1{i}(ind_p2);      %well
             cost_p2 = min(P2{i});                  
-            eql = {eql;[cost_p1 cost_p2]};
+            eql{i+1} =[cost_p1 cost_p2];
         else
             min_p1 = min(P1{i}); %transpose, because by default indexing moves across the columns
             p1_eq_array = [];
             for id = 1:length(min_p1)
                 ind_p1 = find(P1{i}(:,id)==min_p1(id)); %P1 strategies are defined on rows
                 p1_eq_array = [p1_eq_array; [ind_p1 (ones(1,length(ind_p1))*id)']];
-                %zapisywac indeks i numer iteracji: indeks = kolumna a
-                %numer iteracji wiersz (do wektora)
             end
             min_p2 = min(P2{i}'); 
             p2_eq_array = [];
             for id = 1:length(min_p2)
                 ind_p2 = find(P2{i}(:,id)==min_p2(:,id));
                 p2_eq_array = [p2_eq_array; [ind_p2 (ones(1,length(ind_p2))*id)']];
-                %P2 strategies are defined on columns
-                %zapisywac indeks i numer iteracji: indeks = wiersz a
-                %numer iteracji kolumna (do wektora)
             end
-
             equalibrium = find(ismember(p2_eq_array,p1_eq_array, 'rows'));
             P1_out = P1{i}(p2_eq_array(equalibrium(1)),p2_eq_array(equalibrium(1)));
             P2_out = P2{i}(p2_eq_array(equalibrium(1)),p2_eq_array(equalibrium(1)));
             eql{i+1} = [P1_out P2_out];
-            % porównać wektory list, jeżeli któryś się równa to mamy
-            % równowagę 
+
         end
     end
+    eql(1) = [];
 end
